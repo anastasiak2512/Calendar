ebdc5e004debf86afa743a5fbbab2cf03f621f96
Anastasia Kazakova <anastasia.kazakova@jetbrains.com>
2015-10-13 16:13:13 +0300

    commit test fixture for basic checks

--- calendars_tests/basic_tests/basic_check.cpp
+++ calendars_tests/basic_tests/basic_check.cpp
@@ -2,9 +2,12 @@
 // Created by AK on 13/10/15.
 //
 
-#include <iostream>
 #include "gtest/gtest.h"
 
-TEST(test_default, basic_check) {
+TEST(basic_check, test_eq) {
     EXPECT_EQ(1, 1);
+}
+
+TEST(basic_check, test_neq) {
+    EXPECT_NE(1, 0);
 }
---
+++ calendars_tests/basic_tests/calendar_check.cpp
@@ -0,0 +1,49 @@
+//
+// Created by AK on 13/10/15.
+//
+
+#include "gtest/gtest.h"
+
+#include "GregorianCalendar.h"
+#include "IslamicCalendar.h"
+#include "JulianCalendar.h"
+#include "HebrewCalendar.h"
+
+#define CLION_DAY_GR 14
+#define CLION_MONTH_GR 4
+#define CLION_YEAR_GR 2015
+#define CLION_ABSOLUTE 735702
+
+class CalendarFixture : public ::testing::Test {
+protected:
+    virtual void TearDown() {
+    }
+
+    virtual void SetUp() {
+    }
+
+public:
+    CalendarFixture() : Test() {
+        gregorian_calendar = new GregorianCalendar(CLION_MONTH_GR, CLION_DAY_GR, CLION_YEAR_GR);
+    }
+
+    virtual ~CalendarFixture() {
+        delete gregorian_calendar;
+    }
+
+    GregorianCalendar * gregorian_calendar;
+};
+
+TEST_F(CalendarFixture, absolute_check) {
+    int absolute = *gregorian_calendar;
+    EXPECT_EQ(absolute, CLION_ABSOLUTE);
+}
+
+
+TEST_F(CalendarFixture, julian_check) {
+    int absolute = *gregorian_calendar;
+    JulianCalendar julian_calendar(absolute);
+
+    int julian_absolute = julian_calendar;
+    EXPECT_EQ(julian_absolute, absolute);
+}
